074304adab7e06e576a5e91314e8fd6df7268bc2
Danny Gershman <dgershman@securityscorecard.io>
2017-05-31 22:14:28 -0400

    updated documentation + better way to deploy to s3 w/webpack

---
+++ README.md
@@ -0,0 +1,13 @@
+console-web
+===========
+start developing with the local web server
+
+```shell
+npm start
+```
+
+deploying to s3 bucket for https://console.datedonkey.com, you will need awscli.  this will take from `aws configure` set credentials.
+
+```
+npm run build
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,42 @@
+var S3Plugin = require('webpack-s3-plugin')
 var config = {
-   entry: './main.js', // entry point
-   output: {
-         filename: 'index.js', // place where bundled app will be served
+      plugins: [
+            new S3Plugin({
+                  // Exclude uploading of html 
+                  exclude: /.*\.html$/,
+                  // s3Options are required 
+                  s3Options: {
+                        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                        region: 'us-east-1'
+                  },
+                  s3UploadOptions: {
+                        Bucket: 'console.datedonkey.com'
+                  },
+                  cdnizerOptions: {
+                        defaultCDNBase: 'https://console.datedonkey.com'
+                  }
+            })
+      ],
+      entry: './main.js', // entry point
+      output: {
+            filename: 'index.js', // place where bundled app will be served
       },
-   devServer: {
-         inline: true, // autorefresh
-         port: 3000 // development port server
+      devServer: {
+            inline: true, // autorefresh
+            port: 3000 // development port server
       },
-   module: {
-         loaders: [
-            {
-               test: /\.jsx?$/, // search for js files 
-               exclude: /node_modules/,
-               loader: 'babel-loader',
-   query: {
-               presets: ['es2015', 'react'] // use es2015 and react
-            }
-         }
-      ]
-   }
+      module: {
+            loaders: [
+                  {
+                        test: /\.jsx?$/, // search for js files 
+                        exclude: /node_modules/,
+                        loader: 'babel-loader',
+                        query: {
+                              presets: ['es2015', 'react'] // use es2015 and react
+                        }
+                  }
+            ]
+      }
 }
 module.exports = config;
